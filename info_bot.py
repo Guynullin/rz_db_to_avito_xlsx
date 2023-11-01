c67887d90cf2f5e30e98456b9b104b214668c447
Guynullin <artem.gln@gmail.com>
2023-11-01 17:42:42 +0300

    fix old data in send_message

--- info_bot.py
+++ info_bot.py
@@ -1,21 +1,27 @@
 import requests
 import time
+import logging
 from config import BOT_TOKEN, CHAN_TOKEN, SSH_PASSWORD, DB_PASSWORD, CHAT_ID, CHAN_CHAT_ID
 
 def send_message(message:str):
-    flag = 0
+    err = 0
     message = f"<<<RIMZONA to Avito Bot>>>\n{message}".replace(BOT_TOKEN, '<token>')\
         .replace(SSH_PASSWORD, '<pass>').replace(DB_PASSWORD, '<pass>')
     while True:
         try:
+            if err >= 15:
+                logging.error('Stopping the send_message function')
+                break
             url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage?chat_id={CHAT_ID}&text={message}"
-            print(requests.get(url, timeout=60).json())
+            logging.info(requests.get(url, timeout=60).json())
             break
         except requests.exceptions.ConnectionError as connecterr:
-            print(connecterr)
+            logging.error(connecterr)
+            err += 1
             time.sleep(15)
-            flag = 1
+
         except Exception as ex:
-            print(ex)
+            logging.error(ex)
+            err += 1
             time.sleep(15)
         
